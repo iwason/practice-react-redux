471850cd7d78897ad71b5b16e250e3c70b68146d
iwason <j.iwamura1986@gmail.com>
2018-01-24 22:39:50 +0900

    sample Reactアプリ写経

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -24,3 +24,100 @@ document.addEventListener('DOMContentLoaded', () => {
     document.body.appendChild(document.createElement('div')),
   )
 })
+//const helloStyle = { color: 'red', backgroundColor: 'gray',fontSize: '1.4em'};
+const items = [
+  { name: 'aaa', color: 'black' },
+  { name: 'bbb', color: 'green' },
+  { name: 'ccc', color: 'pink' },
+  { name: 'ddd', color: 'yellow' },
+];
+
+const MyButton = () => (
+  <div>
+    <button onClick={() => alert('clicked')}> click me!</button>
+    </div>
+);
+
+
+let textData = '';
+const setTextData = (event) => {
+  textData = event.target.value;
+  if (textData.length > 10) {
+    textData = textData.slice(0, 10);
+  }
+  render();
+
+};
+let nameData = '';
+const setNameData = (event) => {
+  nameData = event.target.value;
+  render();
+};
+
+let colorData = 'black';
+
+const addData = () => {
+  items.push({ name: nameData, color: colorData });
+  nameData = '';
+  render();
+};
+
+const Hello2 = ({ name, color, onDelete }) => (
+  <div>
+    <p>
+      <span style={{ color }}>Hello {name}!</span>
+      <button onClick={() => onDelete()}>Delete</button>
+    </p>
+  </div>
+);
+
+const deleteItem = (index) => {
+  items.splice(index, 1);
+  render();
+};
+
+const MyBox = () => (
+  <div>
+    <input type='text'value={textData} onChange={setTextData} />
+    </div>
+)
+const MyForm = () => (
+  <div>
+    <input type="text" value={nameData} onChange={setNameData} />
+    <button onClick={addData}>Add Data</button>
+  </div>
+);
+const Hello3 = ({name,color}) => (
+
+  <div style={{color}}><p>Hello {name}</p></div>
+);
+
+const Sample = () => (
+  <div>sample component</div>
+)
+
+const App = () => (
+  <div>
+  <MyForm />
+  <ColorList />
+    {items.map((item,index) => (
+    <Hello2 name={item.name} color={item.color} onDelete={() => deleteItem(index)} />
+      ))}
+    <MyBox />
+  </div>
+);
+
+// const render = () => ReactDOM.render(<App />,
+//     document.body.appendChild(document.createElement('div')),);
+// render();
+
+
+const foo = 'abc';
+const bar = { foo ,moge:'mog'}; // same as { foo: foo }
+console.log(bar);
+
+const obj = { first: 'Ebisu', last: 'JS' };
+function printName({ first, last }) { console.log(first, last); }
+printName(obj);
+const f5 = x => x + 1;
+
